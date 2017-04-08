51be519eff9d10e7e97d621a1253802fb861d8f2
hongjh <565983236@qq.com>
2017-04-08 18:49:20 +0800

    修复mktime函数错误

--- upload/admin/shop_config.php
+++ upload/admin/shop_config.php
@@ -29,7 +29,7 @@ else
 
 $sess_id = $GLOBALS['sess']->get_session_id();
 
-$auth = mktime();
+$auth = time();
 $ac = md5($certi_id.'SHOPEX_SMS'.$auth);
 $url = 'http://service.shopex.cn/sms/index.php?certificate_id='.$certi_id.'&sess_id='.$sess_id.'&auth='.$auth.'&ac='.$ac;
 
--- upload/admin/sms_url.php
+++ upload/admin/sms_url.php
@@ -28,9 +28,7 @@ if(isset($GLOBALS['_CFG']['certificate_id']))
 
     $sess_id = $GLOBALS['sess']->get_session_id();
 
-    $auth = mktime();
+    $auth = time();
     $ac = md5($certi_id.'SHOPEX_SMS'.$auth);
     $url = 'http://service.shopex.cn/sms/index.php?certificate_id='.$certi_id.'&sess_id='.$sess_id.'&auth='.$auth.'&ac='.$ac;
 }
-
-?>
