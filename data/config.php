b2f1368112784c10ed1a388a2ae1ff7f470346a8
hongjh <565983236@qq.com>
2017-04-10 17:19:06 +0800

    增加初始化安装信息

---
+++ data/config.php
@@ -0,0 +1,33 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "refactor-ecshop";
+
+// database username
+$db_user   = "root";
+
+// database password
+$db_pass   = "123456";
+
+// table prefix
+$prefix    = "shop_";
+
+$timezone    = "PRC";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'qSDyULCKrqjxBM7aNnpzugA6IweyzcwVRu3PCs/pH4=');
+
+define('OLD_AUTH_KEY', 'qSDyeyzcwxBM7aNnpzugAULCKrqjIw6s/pH4=');
+
+define('API_TIME', '2017-04-10 16:44:12');
